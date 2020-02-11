376e1fdcbe53e9c10077cc19de67e5fc6f562a39
Michelle Hendrix <michelle49@mail.usf.edu>
2020-02-11 16:17:02 -0500

    game

---
+++ homework/game/game/game/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Game
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to The Game Rock Paper Scissors!");
+      Console.WriteLine("You may choose Rock, Paper, or Scissors.");
+      Console.WriteLine("Please choose an option!");
+     var userChoice = Console.ReadLine();
+      
+
+      Random rnd = new Random();
+      
+
+      int mIndex = rnd.Next(userChoice.Length);
+      Console.WriteLine("   userChoice:      {0}", userChoice);
+
+
+      string[] computerChoice = { "Rock", "Paper", "Scissors" };   
+      int fIndex = rnd.Next(computerChoice.Length);
+
+      Console.WriteLine("   Computer Choice:   {0}", computerChoice[mIndex]); 
+
+    
+      
+
+
+    } 
+
+  }
+}
