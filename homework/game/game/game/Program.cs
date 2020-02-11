899b0a79007222b184b6f7001e96ed4785ba0d17
Michelle Hendrix <michelle49@mail.usf.edu>
2020-02-11 16:46:47 -0500

    Game

--- homework/game/game/game/Program.cs
+++ homework/game/game/game/Program.cs
@@ -9,22 +9,52 @@ namespace Game
       Console.WriteLine("Welcome to The Game Rock Paper Scissors!");
       Console.WriteLine("You may choose Rock, Paper, or Scissors.");
       Console.WriteLine("Please choose an option!");
-     var userChoice = Console.ReadLine();
+      var userChoice = Console.ReadLine();
       
 
-      Random rnd = new Random();
       
-
-      int mIndex = rnd.Next(userChoice.Length);
       Console.WriteLine("   userChoice:      {0}", userChoice);
 
 
-      string[] computerChoice = { "Rock", "Paper", "Scissors" };   
-      int fIndex = rnd.Next(computerChoice.Length);
+      Random rnd = new Random();
+      string[] computerChoices = { "Rock", "Paper", "Scissors" };   
+      int fIndex = rnd.Next(computerChoices.Length);
+      var computerChoice = computerChoices[fIndex];
 
-      Console.WriteLine("   Computer Choice:   {0}", computerChoice[mIndex]); 
+      Console.WriteLine("   Computer Choice:   {0}", computerChoice); 
 
-    
+  
+      if (userChoice == "Rock" && computerChoice == "Rock") {
+        Console.WriteLine("It was a tie!");
+      }
+      
+      if (userChoice == "Rock" && computerChoice == "Scissors") { 
+        Console.WriteLine("You Win!!");
+      }
+
+      if (userChoice == "Paper" && computerChoice == "Paper") {
+        Console.WriteLine("It was a tie!");
+      }
+
+      if (userChoice == "Paper" && computerChoice == "Rock") {
+        Console.WriteLine("You Win!!");
+      }
+      if (userChoice == "Scissors" && computerChoice == "Rock") {
+        Console.WriteLine("Computer Wins!");
+      }
+
+      if (userChoice == "Paper" && computerChoice == "Scissors") {
+        Console.WriteLine("Computer Wins!");
+      }
+
+      if (userChoice == "Scissors" && computerChoice == "Paper") {
+        Console.WriteLine("You Win!!");
+      }
+
+      if (userChoice == "Scissors" && computerChoice == "Scissors") {
+        Console.WriteLine("It was a tie!");
+      }
+        
       
 
 
