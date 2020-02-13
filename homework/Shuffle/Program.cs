157aaf7005cadf87ad23040e686302fb2dac4ed0
Michelle Hendrix <michelle49@mail.usf.edu>
2020-02-12 23:12:53 -0500

    add Explorer Mode

--- homework/Shuffle/Program.cs
+++ homework/Shuffle/Program.cs
@@ -8,6 +8,7 @@ namespace Shuffle {
       var deckFace = new List<string> () { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
       var deck = new List<string> ();
 
+      // Fill/create deck
       for (var i = 0; i < deckSuit.Count; i++) {
         for (var j = 0; j < deckFace.Count; j++) {
           var card = ($"{deckFace[j]} of {deckSuit[i]}");
@@ -32,11 +33,40 @@ namespace Shuffle {
         deck[randomIndex] = cardAtCardIndex;
       }
 
+      // Display all cards
       for (int cardIndex = 0; cardIndex < deck.Count; cardIndex++) {
         var card = deck[cardIndex];
         Console.WriteLine (card);
       }
 
+      Console.WriteLine ("---- Explorer Mode ----");
+      // Explorer Mode
+      bool isPlaying = true;
+      int idx = 0;
+      while (isPlaying) {
+        var card = deck[idx];
+        Console.WriteLine (card);
+
+        // You've already played a round
+        Console.WriteLine ("Do you still want to play?");
+        Console.WriteLine ("Enter y/n: ");
+
+        string response = Console.ReadLine ();
+
+        while (response != "n" && response != "y") {
+          Console.WriteLine ("That is not a valid response. Please try again.");
+          Console.WriteLine ("Enter y/n: ");
+          response = Console.ReadLine ();
+        }
+
+        if (response == "n") {
+          isPlaying = false;
+          Console.WriteLine ("Thanks for playing!");
+
+        } else {
+          idx++;
+        }
+      }
     }
   }
 }
